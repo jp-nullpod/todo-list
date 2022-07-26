974f088f120e3b3d8c719ee24bb96644e348d5dc
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2022-07-26 20:17:13 +0400

    deletion implemented. task string converted to javascript object

--- src/App.js
+++ src/App.js
@@ -10,8 +10,19 @@ function App() {
     }
 
     const addTask = () => {
-        const newTodoList = [...todoList, newTask]
-        setTodolist(newTodoList)
+        const task = {
+            id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+            name: newTask
+        };
+
+        setTodolist([...todoList, task])
+    }
+
+    const deleteTask = (id) => {
+        const newList = todoList.filter((task) => {
+            return task.id !== id
+        })
+        setTodolist(newList)
     }
 
     return (
@@ -27,7 +38,13 @@ function App() {
                 <ul>
                     {
                         todoList.map((task) => {
-                            return <li>{task}</li>
+                            return (
+                                <div>
+                                    <li>{task.name} {" "}
+                                        <button onClick={() => deleteTask(task.id)}>X</button>
+                                    </li>
+                                </div>
+                            )
                         })
                     }
                 </ul>
